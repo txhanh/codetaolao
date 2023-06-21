9c393f7cac43a95256dabb8908d65f4bfe156fff
Hanh Tran Xuan <txhanh@MBP-Hanh.lan>
2023-06-22 01:17:04 +0700

    done and export the result, staff_position_log_data

--- multijson.py
+++ multijson.py
@@ -1,3 +1,4 @@
+# Multiply JSON objects
 import json
 
 # Read JSON object from file
@@ -14,3 +15,12 @@ for _ in range(25578):
 # Write the multiplied JSON data to a filecode
 with open('output.json', 'w') as file:
     json.dump(multiplied_data, file)
+
+# export data
+
+data = ["apple", "banana", "orange", "grape"]
+
+# Open the file in write mode
+with open('output.json', 'w') as file:
+    # Write the list to the file in JSON format
+    json.dump(data, file)
